5f70bf824e015bc981ffe5714a539167fbf59fc7
Jon Grimes <nsa@tamu.edu>
2017-12-24 01:21:54 -0600

    Starting to get the add button added

--- CTFd/plugins/discovery/config.js
+++ CTFd/plugins/discovery/config.js
@@ -21,7 +21,9 @@ function loadchals2(){
                         
                        var chalDisc = '<table class="discovery" id="discovery-{0}" value="{1}" cellspacing="0" cellpadding="0" style="display: inline-block;"></table></br></br></br></br>'.format(challenges['game'][k].id, challenges['game'][k].name);
                        $('#challenges2').append($(chalDisc));
-                       $('#discovery-'+challenges['game'][k].id).append('<td><button class="chal-button col-md-2 theme-background" value="{0}"><h5>{1}</h5><p>{2}</p></button></td>'.format(challenges['game'][k].id, challenges['game'][k].name, challenges['game'][k].value));
+                       $('#discovery-'+challenges['game'][k].id).append('<tr><td><button class="chal-button col-md-2 theme-background" value="{0}"><h5>{1}</h5><p>{2}</p></button></td>'.format(challenges['game'][k].id, challenges['game'][k].name, challenges['game'][k].value));
+                      
+                       $('#discovery-'+challenges['game'][k].id).append(builddiscovery(challenges['game'][k].name));
                     }
                 }
 
@@ -238,6 +240,96 @@ function builddiscovery(){
         
     return elem;
 }
+
+function builddiscovery(chal){
+
+    var discoveryList = [];
+    
+   
+    curChalNum = chal;
+
+    
+    var elem = $('<div class="col-md-12 row current-discovery">');
+    discovery_dropdown += 1;
+    var this_disc_drop_id = discovery_dropdown;
+    
+    var buttons = $('<div class="btn-group disc-drop" role="group">');
+    var dropdown = $('<div class="btn-group dropdown" role="group">');
+    dropdown.append('<button class="btn btn-default dropdown-toggle" type="button" id="discovery_dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"><span class="chal-quantity">0</span> Challenges<span class="chal-plural">s</span> <span class="caret"></span></button>');
+    var options = $('<ul class="dropdown-menu" aria-labelledby="discovery_dropdown">');
+    dropdown.append(options);
+    buttons.append(dropdown);
+    
+    $('.chal-button').each(function(){
+        if(this.firstChild.innerText != curChalNum){
+            add_discovery = $('<li class="discovery-item"><a href="#"><span class="fa fa-square-o" aria-hidden="true"></span><span class="fa fa-check-square-o" aria-hidden="true"></span> '+'ID: '+$(this).val()+'| name: '+this.firstChild.innerText+'</a></li>');
+            add_discovery.click(function(e){
+                if($(this).hasClass('active')){
+                    $(this).removeClass('active');
+                    $(this).find('.fa-check-square-o').hide();
+                    $(this).find('.fa-square-o').show();
+                }
+                else{
+                    $(this).addClass('active');
+                    $(this).find('.fa-check-square-o').show();
+                    $(this).find('.fa-square-o').hide();
+                }
+                var numActive = $(this).parent().find(".active").length;
+                
+                discElem=[];
+                discovery=[];
+                for(var i = 0; i < numActive; ++ i){
+                    var optionText = $(this).parent().find(".active")[i].innerText;
+                    if(discElem.indexOf(optionText) == -1){
+                        discElem.push(optionText);
+                    }
+                }
+                $(discElem).each(function(){
+                    discovery.push(parseInt(String(this.match(/(ID:\ )\d+/g)).replace(/(ID:\ )/g, '')));
+                });
+                discovery=discovery.join('&');
+                
+                if (discovery.length > 0){
+                    if($(String('.disc'+this_disc_drop_id)).length == 0){
+                        discovery = "<span class='label label-primary chal-discovery disc"+this_disc_drop_id+"'><span>"+discovery  ;                 
+                        $('#chal-discoveryList').append(discovery);
+                        // $('#chal-discoveryList')[this_disc_drop_id] = discovery;
+                    } else{
+                        $(String('.disc'+this_disc_drop_id))[0].innerText=discovery;
+                    }
+                    $('.discovery-insert').val("");
+                }
+              
+                $(this).parent().parent().find(".chal-quantity").text(numActive.toString());
+                if(numActive == 1){
+                    $(this).parent().parent().find(".chal-plural").html("&nbsp;");
+                }
+                else {
+                    $(this).parent().parent().find(".chal-plural").html("s");
+                }
+                e.stopPropagation();
+            })
+            add_discovery.find('.fa-check-square-o').hide();
+            var chalid = parseInt($(this).find('.chal-button').value);
+            add_discovery.append($("<input class='chal-link' type='hidden'>").val(chalid));
+            options.append(add_discovery);
+
+            if($.inArray(chalid, discoveryList) > -1){
+                add_discovery.click();
+            }
+        }
+    });
+    
+    if(options.children().length == 0){
+      options.append('<li>&nbsp; No other Problems</li>');
+    }
+
+    buttons.append('<a href="#" onclick="$(this).parent().parent().remove(); $(String(\'.disc'+String(this_disc_drop_id)+'\')).remove()" style="margin-right:-10px;" class="btn btn-danger pull-right discovery-remove-button">Remove</a>');
+    elem.append(buttons);
+        
+    return elem;
+}
+
 
 $('#submit-discoveryList').click(function (e) {
     e.preventDefault();
